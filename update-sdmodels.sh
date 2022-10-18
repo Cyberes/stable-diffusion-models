e28b728fbf88229040e4e1b15071820c161573fe
adpkadspokasdk <64224601+Cyberes@users.noreply.github.com>
2022-10-18 13:03:35 -0600

    c

--- update-sdmodels.sh
+++ update-sdmodels.sh
@@ -6,6 +6,15 @@ wget -r -H -l 1 https://rentry.org/sdmodels -e robots=off -U "Mozilla/5.0 (iPhon
 
 mv rentry.org/static ..
 mv rentry.co/sdmodels ../index.html
+mv rentry.org/sdmodels/* ..
 cd ..
-sed -i -e 's/\/static\//static\//g' index.html
+perl -pi -e 's/\/static\//static\//g' index.html
 rm -rf tmp
+
+mv "static/css/bootstrap.min.css?v=84" static/css/bootstrap.min.css
+mv "static/js/bootstrap.min.js?v=20" static/js/bootstrap.min.js
+mv "static/js/jquery.min.js?v=20" static/js/jquery.min.js
+
+perl -pi -e 's/bootstrap\.min\.css\?v=[0-9]*/bootstrap\.min\.css/g' index.html
+perl -pi -e 's/bootstrap\.min\.js\?v=[0-9]*/bootstrap\.min\.js/g' index.html
+perl -pi -e 's/jquery\.min\.js\?v=[0-9]*/jquery\.min\.js/g' index.html
