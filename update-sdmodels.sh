9be740a4bbe134743bf46acf3206e97133391983
adpkadspokasdk <64224601+Cyberes@users.noreply.github.com>
2022-10-18 13:44:27 -0600

    fix

--- update-sdmodels.sh
+++ update-sdmodels.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -rf docs/sdmodels/*
-mkdir -p docs/sdmodels/tmp
-cd docs/sdmodels/tmp
+rm -rf site/sdmodels/*
+mkdir -p site/sdmodels/tmp
+cd site/sdmodels/tmp
 
 wget -r -H -l 1 https://rentry.org/sdmodels -e robots=off -U "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Mobile/15E148 Safari/604.1"
 
