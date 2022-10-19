caf3c0a492dfc4c55d3444555699966322d93c7f
adpkadspokasdk <64224601+Cyberes@users.noreply.github.com>
2022-10-19 10:45:56 -0600

    fix

--- update-sdmodels.sh
+++ update-sdmodels.sh
@@ -21,19 +21,20 @@ mv pdf sdmodels.pdf
 mv raw sdmodels-raw.txt
 wget https://rentry.org/sdmodels/png --timeout=900 -O sdmodels.png
 
-mv "static/css/bootstrap.min.css?v=84" static/css/bootstrap.min.css
-mv "static/js/bootstrap.min.js?v=20" static/js/bootstrap.min.js
-mv "static/js/jquery.min.js?v=20" static/js/jquery.min.js
+mv static/css/bootstrap.min.css?v=* static/css/bootstrap.min.css
+mv static/js/bootstrap.min.js?v=* static/js/bootstrap.min.js
+mv static/js/jquery.min.js?v=* static/js/jquery.min.js
 
 perl -pi -e 's/bootstrap\.min\.css\?v=[0-9]*/bootstrap\.min\.css/g' index.html
 perl -pi -e 's/bootstrap\.min\.js\?v=[0-9]*/bootstrap\.min\.js/g' index.html
 perl -pi -e 's/jquery\.min\.js\?v=[0-9]*/jquery\.min\.js/g' index.html
+perl -pi -e 's/<\/body>/<script src="https:\/\/getbootstrap\.com\/docs\/5\.0\/dist\/js\/bootstrap\.bundle\.min\.js"><\/script><\/body>g/' index.html
 
 perl -pi -e 's/\/sdmodels\/raw/sdmodels-raw\.txt/g' index.html
 perl -pi -e 's/\/sdmodels\/pdf/sdmodels\.pdf/g' index.html
 perl -pi -e 's/\/sdmodels\/png/sdmodels\.png/g' index.html
 
-perl -pi -e 's/<body class="m-0 p-0">/<body class="m-0 p-0">  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color:#6c757d42!important;">     <div class="container-fluid">       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">         <div class="navbar-nav">           <a class="nav-link" href="\/stable-diffusion-models\/">Models<\/a>           <a class="nav-link" href="\/stable-diffusion-textual-inversion-models\/">Textual Inversion Models<\/a>           <a class="nav-link" href="\/stable-diffusion-dreambooth-library\/">DreamBooth Models<\/a>           <a class="nav-link active" aria-current="page" href="\/stable-diffusion-models\/sdmodels\/">sdmodels<\/a>         <\/div>       <\/div>     <\/div>   <\/nav>/g' index.html
+perl -pi -e 's/<body class="m-0 p-0">/<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color:#6c757d42!important"><div class="container-fluid"><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navtogglecontent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"><\/span><\/button><div class="collapse navbar-collapse" id="navtogglecontent"><div class="navbar-nav"><a class="nav-link" href="\/stable-diffusion-models\/">Models<\/a><a class="nav-link" href="\/stable-diffusion-textual-inversion-models\/">Textual Inversion Models<\/a><a class="nav-link" href="\/stable-diffusion-dreambooth-library\/">DreamBooth Models<\/a><a class="nav-link active" aria-current="page" href="\/stable-diffusion-models\/sdmodels\/">sdmodels<\/a><\/div><\/div><\/div><\/nav>/g' index.html
 
 perl -pi -e 's/<\/head>/<script>var _paq=window\._paq=window\._paq\|\|\[\];_paq\.push\(\["trackPageView"\]\),_paq\.push\(\["enableLinkTracking"\]\),_paq\.push\(\["enableHeartBeatTimer"\]\),_paq\.push\(\["trackVisibleContentImpressions"\]\),function\(\){var e="https:\/\/mato\.evulid\.cc\/";_paq\.push\(\["setTrackerUrl",e\+"matomo\.php"\]\),_paq\.push\(\["setSiteId","1"\]\);var a=document,t=a\.createElement\("script"\),p=a\.getElementsByTagName\("script"\)\[0\];t\.async=!0,t\.src=e\+"matomo\.js",p\.parentNode\.insertBefore\(t,p\)}\(\);<\/script><\/head>/g' index.html
 
